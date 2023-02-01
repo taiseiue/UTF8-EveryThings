b9ddeac9ad203be0b0a1e2936d14df6008b8594e
taiseiue <taisei@wsoft.gq>
2023-02-01 09:59:57 +0900

    初めてのコミット

---
+++ UTF8-EveryThings/ParsedArguments.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTF8_EveryThings
+{
+    class ParsedArguments
+    {
+        public ParsedArguments(string[] args)
+        {
+            m_init(args);
+        }
+        public ParsedArguments(string args)
+        {
+            m_init(args.Split(new string[] { "\r\n" }, StringSplitOptions.None));
+        }
+        private void m_init(string[] args)
+        {
+            bool aarg = false;
+            foreach (string arg in args)
+            {
+                if (arg.ToLower() == "--arg")
+                {
+                    aarg = true;
+                    continue;
+                }
+                if (aarg)
+                {
+                    Args.Add(arg);
+                }
+                else
+                {
+                    System.Text.RegularExpressions.MatchCollection mc =
+        System.Text.RegularExpressions.Regex.Matches(
+        arg, @"-.*");
+                    if (mc.Count > 0)
+                    {
+                        foreach (System.Text.RegularExpressions.Match m in mc)
+                        {
+                            System.Text.RegularExpressions.MatchCollection mc2 =
+            System.Text.RegularExpressions.Regex.Matches(
+            arg, @"-.*:.*");
+                            if (mc2.Count > 0)
+                            {
+                                foreach (System.Text.RegularExpressions.Match m2 in mc2)
+                                {
+                                    string v = m2.Value;
+                                    v = v.TrimStart('-'); string[] vs = v.Split(':');
+                                    Values.Add(vs[0].ToLower(), vs[1]);
+                                }
+                            }
+                            else
+                            {
+                                string v = m.Value;
+                                v = v.TrimStart('-');
+                                Flags.Add(v.ToLower());
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Files.Add(arg);
+                    }
+                }
+            }
+        }
+        private Dictionary<string, string> m_values = new Dictionary<string, string>();
+        public List<string> Args = new List<string>();
+        public Dictionary<string, string> Values
+        {
+            get
+            {
+                return m_values;
+            }
+            set
+            {
+                m_values = value;
+            }
+        }
+        private List<string> m_flags = new List<string>();
+        public List<string> Flags
+        {
+            get
+            {
+                return m_flags;
+            }
+            set
+            {
+                m_flags = value;
+            }
+        }
+        private List<string> m_files = new List<string>();
+        public List<string> Files
+        {
+            get
+            {
+                return m_files;
+            }
+            set
+            {
+                m_files = value;
+            }
+        }
+    }
+}
--- UTF8-EveryThings/Program.cs
+++ UTF8-EveryThings/Program.cs
@@ -1,2 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hnx8.ReadJEnc;
+using System.Reflection;
+using System.Text;
+
+namespace UTF8_EveryThings
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var arg = new ParsedArguments(args);
+            if(arg.Values.Keys.Contains("enc"))
+            {
+                OutputEncording = Encoding.GetEncoding(arg.Values["-enc"]);
+            }
+            SilentMode = arg.Flags.Contains("silent");
+
+            Print("UTF-EveryThings バージョン"+Assembly.GetExecutingAssembly().GetName().Version.ToString());
+            Print("(c)Copylight 2022 WSOFT All rights reserved.");
+
+            if (arg.Flags.Contains("analyze"))
+            {
+                //文字コード判定モード
+                AnalyzeFile(arg.Files[0]);
+                return;
+            }
+            if (arg.Flags.Contains("cat"))
+            {
+                //ファイル出力モード
+                AnalyzeFile(arg.Files[0],true);
+                return;
+            }
+
+            if (arg.Files.Count() > 1)
+            {
+                //入力と出力は異なるファイル
+                if (SaveUTF8TextFile(arg.Files[0], arg.Files[1]))
+                {
+                    Print("[成功]変換済みのファイルを" + arg.Files[1] + "に書き出しました。");
+                }
+                else
+                {
+                    Print("[失敗]ファイルの変換に失敗しました。");
+                }
+            }
+            else if(arg.Files.Count > 0)
+            {
+                //入力と出力は同じファイル
+                if (SaveUTF8TextFile(arg.Files[0], arg.Files[0]))
+                {
+                    Print("[成功]変換済みのファイルで" + arg.Files[0]+"を置換しました。");
+                }
+                else
+                {
+                    Print("[失敗]ファイルの変換に失敗しました。");
+                }
+            }
+            else
+            {
+                Console.WriteLine("UTF8-EveryThings 使用法");
+                Console.WriteLine("utf8 ファイル名  : ファイルをUTF8形式のテキストファイルで置換します。");
+                Console.WriteLine("utf8 ファイル名 出力先  : ファイルを読み込み、UTF8形式のテキストファイルに出力します。");
+                Console.WriteLine("utf8 ファイル名  -enc:shift-jis : 指定した文字コードでテキストファイルを置換します。");
+                Console.WriteLine("utf8 ファイル名  -cat : 指定したファイルを読み込み、内容を出力します。");
+                Console.WriteLine("utf8 ファイル名  -analyze : 指定したファイルを読み込み、文字コードを出力します。");
+                Console.WriteLine("utf8 ファイル名  -silent : 操作時に余分な出力をしません。");
+            }
+        }
+        private static Encoding OutputEncording { get; set; } = Encoding.UTF8;
+        private  static void AnalyzeFile(string target,bool cat=false)
+        {
+            var file = new FileInfo(target);
+            if (file.Exists)
+            {
+                using (FileReader reader = new FileReader(file))
+                {
+                    Hnx8.ReadJEnc.CharCode c = reader.Read(file);
+                    Print("検出された文字コードまたはファイル種別 : " + c.Name);
+                    string name = c.Name;
+                    string text = reader.Text;
+                    if (cat && text != null)
+                    {
+                        Print("-----------------------------");
+                        Console.WriteLine(text);
+                    }
+                }
+            }
+        }
+        private static bool SaveUTF8TextFile(string from, string to)
+        {
+            var file = new FileInfo(from);
+            if (file.Exists)
+            {
+                using (FileReader reader = new FileReader(file))
+                {
+                    Hnx8.ReadJEnc.CharCode c = reader.Read(file);
+                    Print("検出された文字コードまたはファイル種別 : " + c.Name);
+                    string name = c.Name;
+                    string text = reader.Text;
+                    if (text != null)
+                    {
+                        File.WriteAllText(to, text,OutputEncording);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private static  bool SilentMode { get; set; }
+        private  static void Print(string text)
+        {
+            if (!SilentMode)
+            {
+                Console.WriteLine(text);
+            }
+        }
+    }
+}
